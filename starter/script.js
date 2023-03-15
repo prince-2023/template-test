6b4c435c9de120036dea781bb5d3136e7f68f142
prince-2023 <2005.prince@gmail.com>
2023-03-16 01:06:24 +0530

    initial push

---
+++ starter/script.js
@@ -0,0 +1,58 @@
+"use strict";
+
+// Data needed for a later exercise
+const flights =
+  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
+
+// Data needed for first part of the section
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+};
+
+const { name, starterMenu, openingHours } = restaurant;
+console.log(name, starterMenu, openingHours);
+
+const {
+  name: restaurantName,
+  starterMenu: first,
+  openingHours: tags,
+} = restaurant;
+console.log(restaurantName, first, tags);
+
+const caclTempAmplitude = function (arr) {
+  let max = arr[0];
+  let min = arr[0];
+  arr.forEach(item => {
+    const curTemp = item;
+    if (max < item) {
+      max = item;
+    }
+    if (min > item) {
+      min = item;
+    }
+
+    return max - min;
+  });
+};
+
+const temperature = [3, 34, 54, 32, "error", 40];
+console.log(caclTempAmplitude(temperature));
